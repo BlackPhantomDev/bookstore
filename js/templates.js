b4eae742c382b894126b66c4281033ee6807efef
Mischa Siegrist <therealphantom22@proton.me>
2025-12-07 13:59:20 +0100

    all template functions

---
+++ js/templates.js
@@ -0,0 +1,85 @@
+let comments = document.getElementById('book_comments');
+
+function bookCommentsTemplateHtml(commentUsername, commentContent) {
+    return `
+        <div class="comment">
+            <span>[${commentUsername}]</span>
+            <span>${commentContent}</span>
+        </div>
+    `;
+}
+
+function checkLiked(liked, bookId) {
+    let like_img_id = "like_btn_img_" + bookId;
+    let likeBtnImg = "";
+
+    if (liked == true){
+        likeBtnImg = '<img id="'+like_img_id+'" class="like_btn_img" src="./assets/icons/heart_empty.png" alt="Like button">';
+    }else {
+        likeBtnImg = '<img id="'+like_img_id+'" class="like_btn_img" src="./assets/icons/heart_filled.png" alt="Like button">';
+    }
+    
+    return likeBtnImg;
+}
+
+function getBookTemplate(title, author, likes, liked, price, publishedYear, genre, comments, bookId) {
+    let commentsHTML = "";
+    
+    likeBtnImg = checkLiked(liked, bookId);
+    
+    for (let index = 0; index < comments.length; index++) {
+        let commentUsername = comments[index].name;
+        let commentContent = comments[index].comment;
+        
+        commentsHTML += bookCommentsTemplateHtml(commentUsername, commentContent);
+    }
+
+    return `
+        <div class="book" id="${bookId}">
+            <section id="book_title">
+                <h3>${title}</h3>
+            </section>
+
+            <section id="book_img">
+                <img src="./assets/img/book.png" alt="Book img">
+            </section>
+
+            <section id="book_info_section">
+                <div id="price_likes">
+                    <p>${price} €</p>
+                    <p><span id="likes_${bookId}">${likes}</span> <button id="like_btn" onclick="likeToggle(${bookId})">${likeBtnImg}</button></p>
+                </div>
+                <div id="book_infos">
+                    <table>
+                        <tr>
+                            <td>Author:</td>
+                            <td id="author">${author}</td>
+                        </tr>
+                        <tr>
+                            <td>Publish year:</td>
+                            <td id="publish_year">${publishedYear}</td>
+                        </tr>
+                        <tr>
+                            <td>Genre:</td>
+                            <td id="genre">${genre}</td>
+                        </tr>
+                    </table>
+                </div>
+            </section>
+
+            <section id="book_comment_section">
+                <div id="book_comments">
+
+                ${commentsHTML}
+                    
+                </div>
+                <form action="#commented">
+                    <input id="comment_input" type="text" placeholder="Schreibe einen Kommentar...">
+                    <button id="comment_button"><img src="./assets/icons/send.png" alt=""></button>
+                </form>
+
+                </section>
+
+            </div>
+    `;
+}
